b60a0d403ef0254207b7dcdb583ca2b5ab031ab4
BH92 <babak.habibnejad@gmail.com>
2024-12-22 14:40:40 +0100

    Update CKA-staticPods.sh

--- Kubernetes/CKA-staticPods.sh
+++ Kubernetes/CKA-staticPods.sh
@@ -5,23 +5,24 @@ k run static-pod --image=busybox --dry-run=client -o yaml --command -- sleep 200
 cp spod.yaml /etc/kubernetes/manifests/
 # or
 k run static-busybox --image=busybox --dry-run=client -o yaml --command -- sleep 2000 > /etc/kubernetes/manifests/pod.yaml
+# it might take 1-2 minutes until pod is automatically created
 
 # Create a kubectl command that lists out all static pod names currently running within the cluster. Run the kubectl command and save the output directly into 
 # the following file /home/ubuntu/static-pods/report.txt on the bastion node.
 k get po -A
-# if a pod's own references has kind "NODE" it means that its static pod
-k get po -A -o=custom-columns=NAME:.metadata.name,STATIC:.metadata.ownReferences[*].kind | grep NODE
+# if a pod's "ownReferences.kind" has kind "NODE" it means that its static pod
+k get po -A -o=custom-columns="NAME:.metadata.name,STATIC:.metadata.ownReferences[*].kind" | grep NODE
 vi /home/ubuntu/static-pods/report.txt   # write the names here
 
-  apiVersion: v1
-  kind: Pod
-  metadata:
-    annotations:
-      cni.projectcalico.org/podIP: 192.168.203.136/32
-    name: blah-01-ip-10-0-0-10.us-west-2.compute.internal
-    ownerReferences:
-    - apiVersion: v1
-      kind: Node
+# apiVersion: v1
+# kind: Pod
+# metadata:
+#   annotations:
+#     cni.projectcalico.org/podIP: 192.168.203.136/32
+#   name: blah-01-ip-10-0-0-10.us-west-2.compute.internal
+#   ownerReferences:
+#   - apiVersion: v1
+#     kind: Node
 
 # move static pod from one node to another
 ssh controlplane
@@ -29,8 +30,8 @@ scp /etc/kubernetes/manifests/my-sp.yaml node01:/etc/kubernetes/manifests/my-sp.
 
 # schedule a pod ONLY on controlplane node => node selector (label) + tiant-toleration (taints)
 k get node
-k describe node cluster1-controlplane1 | grep Taint -A1 # node-role.kubernetes.io/control-plane:NoSchedule
-k get node cluster1-controlplane1 --show-labels # node-role.kubernetes.io/control-plane: ""
+k describe node cluster1-controlplane1 | grep Taint -A1 # node-role.kubernetes.io/control-plane:NoSchedule  => for toleration
+k get node cluster1-controlplane1 --show-labels # node-role.kubernetes.io/control-plane: "" => for nodeSelector
 k run pod1 --image=httpd:2.4.41-alpine --dry-run=client -o yaml > 2.yaml
 
 apiVersion: v1
@@ -45,8 +46,9 @@ spec:
     name: pod1-container
   # toleration allows the pod to run on the controlplane
   tolerations:                                 
-  - effect: NoSchedule                         
+  - effect: NoSchedule    
+    operator: Exists
     key: node-role.kubernetes.io/control-plane 
-  # nodeselector chooses the pod based on the label
+  # nodeselector chooses the pod based on the label (key-value)
   nodeSelector:                                
     node-role.kubernetes.io/control-plane: ""  
