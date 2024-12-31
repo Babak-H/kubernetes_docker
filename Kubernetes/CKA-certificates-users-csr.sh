6bfa403328c673551fc5d9dffc26f34e7d4f697e
BH92 <babak.habibnejad@gmail.com>
2024-12-31 10:17:41 +0100

    Update CKA-certificates-users-csr.sh

--- Kubernetes/CKA-certificates-users-csr.sh
+++ Kubernetes/CKA-certificates-users-csr.sh
@@ -108,5 +108,85 @@ openssl x509 -req -in admin.csr -CA ca.crt -CAkey ca.key -out admin.crt
 openssl genrsa -out apiserver.key 2048
 # certificate signing request, use the private key
 openssl req -key apiserver.key -subj “/CN=kube-apiserver” -out apiserver.csr -config openssl.cnf
+
+
+# Certificates
+
+# What is the Common Name (CN) configured on the Kube API Server Certificate?
+cat /etc/kubernetes/pki/apiserver.crt
+openssl x509 -in /etc/kubernetes/pki/apiserver.crt -text -noout
+                  # Certificate:
+                  #     Data:
+                  #         Version: 3 (0x2)
+                  #         Serial Number: 3378315762146779274 (0x2ee230495ddca08a)
+                  #         Signature Algorithm: sha256WithRSAEncryption
+                  #         Issuer: CN = kubernetes  #####################
+                  #         Validity 
+                  #             Not Before: Nov 16 14:16:29 2024 GMT
+                  #             Not After : Nov 16 14:21:29 2025 GMT  #####################
+                  #         Subject: CN = kube-apiserver
+                  #         Subject Public Key Info:
+                  #             Public Key Algorithm: rsaEncryption
+                  #                 Public-Key: (2048 bit)
+                  #                 Modulus:
+                  #                     00:...
+                  #                     bc:17
+                  #                 Exponent: 65537 (0x10001)
+                  #         X509v3 extensions:
+                  #             X509v3 Key Usage: critical
+                  #                 Digital Signature, Key Encipherment
+                  #             X509v3 Extended Key Usage:
+                  #                 TLS Web Server Authentication
+                  #             X509v3 Basic Constraints: critical
+                  #                 CA:FALSE
+                  #             X509v3 Authority Key Identifier: A1:...
+                  #             X509v3 Subject Alternative Name:
+                  #                 DNS:controlplane, DNS:kubernetes, DNS:kubernetes.default, DNS:kubernetes.default.svc, DNS:kubernetes.default.svc.cluster.local, IP Address:10.96.0.1, IP Address:192.15.155.9
+                  #     Signature Algorithm: sha256WithRSAEncryption
+                  #     Signature Value: 65:...
+                  
+# How long, from the issued date, is the Kube-API Server Certificate valid for?
+#  Validity
+#       Not Before: Nov 16 14:16:29 2024 GMT
+#       Not After : Nov 16 14:21:29 2025 GMT
+
+# How long, from the issued date, is the Root CA Certificate valid for?
+# --client-ca-file=/etc/kubernetes/pki/ca.cert
+ls /etc/kubernetes/pki/
+openssl x509 -in /etc/kubernetes/pki/ca.crt -text --noout
+#    Validity
+#         Not Before: Nov 16 14:16:29 2024 GMT
+#         Not After : Nov 14 14:21:29 2034 GMT
+
+# What is the Common Name (CN) configured on the ETCD Server certificate?
+cat /etc/kubernetes/pki/etcd/server.crt
+openssl x509 -in /etc/kubernetes/pki/etcd/server.crt -text -noout
+# cn = etcd-ca
 # here we use the public and private keys from certificate authority, and sign the request
 openssl x509 -req -in apiserver.csr -CA ca.crt -CAkey ca.key -out apiserver.crt
+
+
+# Kubectl suddenly stops responding to your commands. Check it out! Someone recently modified the /etc/kubernetes/manifests/etcd.yaml file
+cat  /etc/kubernetes/manifests/etcd.yaml
+ls -l /etc/kubernetes/pki/etcd/server* | grep .crt
+# -rw-r--r-- 1 root root 1208 Nov 16 14:21 /etc/kubernetes/pki/etcd/server.crt
+# Update the "etcd.yaml" YAML file with the correct certificate path and wait for the ETCD pod to be recreated. wait for the kube-apiserver to get to a Ready state.
+
+# The kube-apiserver stopped again! Check it out. Inspect the kube-apiserver logs and identify the root cause and fix the issue.
+# Run "docker ps -a" command to identify the kube-apiserver container. Run "docker logs <container-id>" command to view the logs
+docker ps -a | grep kube-apiserver
+docker logs --tail=2 1fb242055cff8
+# clientconn.go:1331] [core] grpc: addrConn.createTransport failed to connect to {127.0.0.1:2379 127.0.0.1 <nil> 0 <nil>}. Err: connection error: desc = "transport: authentication handshake failed: x509: certificate signed by unknown authority". Reconnecting...
+# run.go:74] "command failed" err="context deadline exceeded"
+docker ps -a | grep etcd
+docker logs --tail 1f24332055cfvnv  # you can see connection errors here
+
+# "127.0.0.1:2379" is address of ETCD server
+# This indicates an issue with the "ETCD CA certificate" used by the kube-apiserver. Correct it to use the file /etc/kubernetes/pki/etcd/ca.crt
+vi /etc/kubernetes/manifests/kube-apiserver.yaml
+# - --etcd-cafile=/etc/kubernetes/pki/etcd/ca.crt
+
+# The path to certificate is incorrect in the kubeconfig file. Correct the certificate name which is available at /etc/kubernetes/pki/users/
+# Inspect the environment and identify the authorization modes configured on the cluster.
+k describe pod kube-apiserver-controlplane -n kube-system | grep auth
+#  --authorization-mode=Node,RBAC
